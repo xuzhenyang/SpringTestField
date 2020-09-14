246906823741044dd08aff9594882204c5600046
lilpilot <master@lilpilot.co>
2020-09-14 21:42:11 +0800

    init project

---
+++ src/main/java/co/lilpilot/springtestfield/SpringTestFieldApplication.java
@@ -0,0 +1,13 @@
+package co.lilpilot.springtestfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestFieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestFieldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/lilpilot/springtestfield/SpringTestFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lilpilot.springtestfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestFieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
